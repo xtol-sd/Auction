a8e2f20ee40de67a486569529d4ef2cfa1166553
Charity Howell <caccheme@hotmail.com>
2013-06-26 08:41:40 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auction::Application.config.secret_token = 'aa5fdfa5be36305c18fd8dc2567a6b1ac48c261a35c6039b68cf91d6a9cc4ab3de8bdd3127c8bfa2862994fe0e9c8857b7acf8bee91b6ef830435cf7c341e5b5'
