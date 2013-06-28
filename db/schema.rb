b0d9cdbeed268fb9ca06ca6c72fa7f1bee5549c8
Charity Howell <caccheme@hotmail.com>
2013-06-27 23:36:36 -0500

    change bid amount to :decimal

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130628041503) do
+ActiveRecord::Schema.define(:version => 20130628043247) do
 
   create_table "bids", :force => true do |t|
     t.integer  "user_id"
     t.integer  "item_id"
-    t.integer  "bid_amount"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.decimal  "bid_amount"
   end
 
   create_table "items", :force => true do |t|
