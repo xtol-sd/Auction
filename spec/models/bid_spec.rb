bd95538f85e0782c718c51a2fb0989495a7ac048
aeriath <aeriath@gmail.com>
2013-07-06 11:00:35 +0900

    started bid specs and added presence validation for bid

--- spec/models/bid_spec.rb
+++ spec/models/bid_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Bid do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) {FactoryGirl.create(:user) }
+  let(:item) {FactoryGirl.create(:item) }
+  before do
+    @bid = user.bids.build( item_id: item.id, bid_amount: "130")
+  end
+  
+  subject { @bid }
+  it { should respond_to(:bid_amount) }
+  it { should respond_to(:item_id) }
+  it { should respond_to(:user_id) }
+
+  it { should be_valid }
+
+  describe "when user_id is not present" do
+    before { @bid.user_id = nil }
+    it { should_not be_valid }
+  end
 end
+
