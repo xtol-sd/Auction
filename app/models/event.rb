9336df8587fff01e2c2a577ce837ebcf71865867
Charity Howell <caccheme@hotmail.com>
2013-07-11 09:29:01 -0500

    refactoring notes for later and update item image to link

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ActiveRecord::Base
   attr_accessible :end_datetime, :name, :start_datetime, :current
   has_many :items
 
+#need to test the item bid information partial for when the auction ends
   def finished?
   	end_datetime <= DateTime.now
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -18,16 +18,15 @@ class Item < ActiveRecord::Base
       top_bid.user_id == current_user_id
     end 
   end
-
+#this returns the top bid record, looks for the top bid amount row...not the particular bid value
   def top_bid
     bids(:bid_amount).max
-    #self.bids.map(&:bid_amount).max || self.start_bid
   end
-
+#do we need self in this next method since it is in item model already? and called by item.top_bid_amount
   def top_bid_amount
     self.bids.map(&:bid_amount).max || self.start_bid
   end
-
+#next two methods should be deleted after my bids refactor
   def bid_upon?(item)
     item_bids = Bid.find_all_by_item_id(item.id)
     if !item_bids.nil?
@@ -41,7 +40,7 @@ class Item < ActiveRecord::Base
       true
     end
   end
-
+#next two replaced by top bid and top bid amount, delete now?
   def highest_bid(current_bid)
   	  bids.each do |bid|
   		  if bid.bid_amount >= current_bid
@@ -63,7 +62,7 @@ class Item < ActiveRecord::Base
   	end
    my_bid
   end
-
+#replaced by top_bid_me? method at top of this model...can delete now?
   def current_user_won?(current_user)
     if self.event.end_datetime < DateTime.now 
       if my_highest_bid(current_user, id, self.start_bid) == highest_bid(start_bid)      
