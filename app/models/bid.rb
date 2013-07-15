6ba8d7817c555c08caf3e9fdb7d37fff568b4ecc
aeriath <aeriath@gmail.com>
2013-07-15 22:05:45 +0900

    removed attr accessible user_id from bid model

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,5 +1,5 @@
 class Bid < ActiveRecord::Base
-  attr_accessible :bid_amount, :item_id, :user_id #user_id not in bid table
+  attr_accessible :bid_amount, :item_id
   belongs_to :user
   belongs_to :item
   accepts_nested_attributes_for :user
--- spec/features/bids_spec.rb
+++ spec/features/bids_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 
-describe "Bids" do
+describe "Create a bid" do
   
   let!(:event) {FactoryGirl.create(:event) }
   let!(:item) {FactoryGirl.create(:item) }
@@ -9,7 +9,7 @@ describe "Bids" do
   
   subject {page}
 
-  describe "Event Show Page Form" do
+  describe "on the Event Show Page" do
     
     before do 
       visit new_user_session_path
@@ -42,9 +42,19 @@ describe "Bids" do
         page.should have_content "Please review the problems below:"
       end
     end
+
+    describe "when the event is finished" do
+      before do
+        event.end_datetime = DateTime.now - 1.day
+        visit event_path(event)
+      end
+      it "page should not display a bid form" do
+        page.should_not have_content 'Bid'
+      end
+    end 
   end
   
-  describe "My Bids Page" do
+  describe "On the My Bids Page" do
     before do 
       visit new_user_session_path
       fill_in "Email",    with: user.email.upcase
@@ -57,17 +67,17 @@ describe "Bids" do
     it {should have_content "My Bids"}
     it {should have_content "Current Top Bidder"}
 
-    describe "not the highest bidder" do
+    describe "when not the top bidder" do
       let(:user2) {FactoryGirl.create(:user, name: "Bidder 2", email: "bidder2@example.com")}
       before do 
-        FactoryGirl.create(:bid, bid_amount: 131, user_id: 2 )
+        FactoryGirl.create(:bid, bid_amount: 135, user_id: 2 )
         visit _my_bids_path 
       end
       it {should have_content "Not Top Bidder"}
       
-      describe "raising the bid" do
+      describe "can raise the bid" do
         before do
-          fill_in "bid_bid_amount", with: '132'
+          fill_in "bid_bid_amount", with: 140
           click_button "Bid"
         end
         it {should have_content "Current Top Bidder"}
