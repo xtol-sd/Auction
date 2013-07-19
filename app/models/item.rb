cb1dba9850567df9cce511d27ba76e27f2fba8d5
Charity Howell <caccheme@hotmail.com>
2013-07-18 23:02:23 -0500

    refactor my bids view and remove old item.rb methods

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,7 +19,7 @@ class Item < ActiveRecord::Base
 
   validates_presence_of :title, :description, :start_bid
 
-  #will need to be changed when more than one photo is implemented
+  #will need to change next two methods when more than one photo is implemented
   def find_thumb_photo_url
     self.photos.each do |photo|
       return photo.image_url(:thumb)
@@ -33,7 +33,6 @@ class Item < ActiveRecord::Base
   end
 
   def next_allowed_bid
-    # if then statement abbreviation
     offset = !bids.empty? ? 5:0
     next_allowed_bid = top_bid_amount + offset
   end
@@ -49,53 +48,9 @@ class Item < ActiveRecord::Base
   def top_bid
     bids(:bid_amount).max
   end
-#do we need self in this next method since it is in item model already? and called by item.top_bid_amount
+
   def top_bid_amount
     self.bids.map(&:bid_amount).max || self.start_bid
-  end
-#next two methods should be deleted after my bids refactor
-  def bid_upon?(item)
-    item_bids = Bid.find_all_by_item_id(item.id)
-    if !item_bids.nil?
-      true
-    end  
-  end
-
-  def no_bids?(item)
-    item_bids = Bid.find_all_by_item_id(item.id)
-    if item_bids.empty?
-      true
-    end
-  end
-#next two replaced by top bid and top bid amount, delete now?
-  def highest_bid(current_bid)
-  	  bids.each do |bid|
-  		  if bid.bid_amount >= current_bid
-  			  current_bid = bid.bid_amount
-  		  end
-  	  end
-    current_bid
-  end
-
-  def my_highest_bid(current_user, item_id, my_bid)
-  	  bids.each do |bid|
-  	  	if current_user.id == bid.user_id 
-  	  		if item_id == bid.item_id
-  		  		if bid.bid_amount >= my_bid
-  			  		my_bid = bid.bid_amount
-  		 		end
-  	  		end
-  		end
-  	end
-   my_bid
-  end
-#replaced by top_bid_me? method at top of this model...can delete now?
-  def current_user_won?(current_user)
-    if self.event.end_datetime < DateTime.now 
-      if my_highest_bid(current_user, id, self.start_bid) == highest_bid(start_bid)      
-        true
-      end
-    end
   end
 
 end
