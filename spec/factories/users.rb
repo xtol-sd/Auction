33f4973d8fc634d3b17c4315f2b111f7054a4279
Charity Howell <caccheme@hotmail.com>
2013-07-24 09:34:37 -0500

    add test that when an item is approved 'approved' appears on reivew items page

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,5 +8,11 @@ FactoryGirl.define do
     password_confirmation 'password'
     # required if the Devise Confirmable module is used
     # confirmed_at Time.now
+
+  factory :admin do
+    after(:create) {|user| user.add_role(:admin)}
   end
+
+  end
+
 end
--- spec/features/items_spec.rb
+++ spec/features/items_spec.rb
@@ -7,19 +7,19 @@ describe "Items" do
   let!(:item) {FactoryGirl.create(:item) }
   let!(:donation) {FactoryGirl.create(:donation) }
   let(:user) {FactoryGirl.create(:user) }
-  
+  let(:admin) {FactoryGirl.create(:admin)}
+
   subject {page}
 
   describe "when item is approved, Approval appears" do
     before do 
       visit new_user_session_path
-      fill_in "Email",    with: user.email.upcase
-      fill_in "Password", with: user.password
+      fill_in "Email",    with: admin.email.upcase
+      fill_in "Password", with: admin.password
       click_button "Login"
       visit donations_path
-
     end
-    it { should have_content 'Pending'}
-   end
+    it { should have_content 'Approved'}
+  end
   
 end
