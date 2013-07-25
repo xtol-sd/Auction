2b5387e8f0836c0ed1f8a1795a6e47db9abc7566
Cindy Rubin <mjmrubin@aol.com>
2013-07-24 22:07:43 -0400

    added 2 tests to items_spec

--- spec/features/items_spec.rb
+++ spec/features/items_spec.rb
@@ -21,5 +21,34 @@ describe "Items" do
     end
     it { should have_content 'Approved'}
   end
+
+  describe "on the My Donations Page" do
+      
+      before do
+        visit new_user_session_path
+        fill_in "Email",    with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Login"
+        visit _my_donations_path
+      end
+
+      it {should have_content "My Donations"}
+
+  end
+
+  describe "on the Review Items Page" do
+      
+      before do
+        visit new_user_session_path
+        fill_in "Email",    with: admin.email.upcase
+        fill_in "Password", with: admin.password
+        click_button "Login"
+        visit donations_path
+      end
+
+      it {should have_content "Review Items"}
+
+  end
+
   
 end
