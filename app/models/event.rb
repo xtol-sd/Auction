dcbf67d3713de4825055164cd728112b204c08d3
Cindy Rubin <cindyrubin13@gmail.com>
2013-07-30 21:16:01 -0400

    added status report and topbid report to dashboard page

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,32 @@ class Event < ActiveRecord::Base
   def finished?
   	end_datetime <= DateTime.now
   end
+
+  def select_all(report)
+  	ActiveRecord::Base.connection.select_all(report)
+  end
+
+  def status_report
+  select_all("select i.title, i.start_bid, u.name, u.email,
+    (CASE WHEN (d.status=0) THEN 'PENDING'
+    WHEN (d.status=1) THEN 'APPROVED'
+    WHEN (D.status=2) THEN 'REJECTED' END) as STATUS
+    from donations d
+    left outer join items i on i.id=d.item_id
+    left outer join users u on u.id=d.user_id
+    group by i.id")
+  end
+  
+  def top_bid_report
+  	select_all("select i.title, i.start_bid, count(b.id) as number_of_bids,
+    MAX(bid_amount) as highest_bid,
+    u.name as highest_bidder,
+    u.email as bidder_email
+    from items i
+    left outer join bids b on i.id = b.item_id
+    left outer join users u on u.id = b.user_id
+    where b.item_id = i.id
+    group by i.id")
+  end
+
 end
