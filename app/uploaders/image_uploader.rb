1ad6b7ec1ca31c48b1a415fe5fc74a1b1c1f5088
Crystal08 <Crystal08@compuserve.com>
2013-08-06 19:52:52 -0600

    Fixes bug of S3 images not appearing through adding back the store_dir method in image_uploader.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -20,9 +20,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  #def store_dir
-  #  "assets/images/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  #end
+  def store_dir
+    "assets/images/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 
   def default_url
     [version_name, "default_image.png"].compact.join('_')
