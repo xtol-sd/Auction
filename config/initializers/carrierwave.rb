b5aa732afb29e5d671ccd18a2cb0e9737727279c
Crystal08 <Crystal08@compuserve.com>
2013-08-08 16:22:00 -0600

    Adds region to carrierwave initializer. Heroku config vars updates to specify region too. Still working on getting heroku app synced up with S3 service.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,6 +3,7 @@ CarrierWave.configure do |config|
   provider: "AWS",
   aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
   aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+  region: ENV['S3_REGION']
   }
   
   config.cache_dir = "#{Rails.root}/tmp/uploads"
